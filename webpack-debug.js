f2d8c2477ca4814e6aea542e0f4a3c1b8370e2fc
rajeevkumar <rajeevkumar@newrelic.com>
2026-07-27 10:18:36 +0530

    fix: Remove webpack-debug.js file

--- webpack-debug.js
+++
@@ -1,6 +0,0 @@
-const cfg = require('./webpack.config.js');
-const vegaCjs = cfg.find(c => c.output && c.output.path && c.output.path.endsWith('cjs/vega'));
-vegaCjs.optimization = { minimize: false };
-vegaCjs.mode = 'development';
-vegaCjs.devtool = false;
-module.exports = vegaCjs;
